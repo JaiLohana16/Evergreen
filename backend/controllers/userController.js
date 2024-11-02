faf62538ee6b8a9126d027517629f1dcb0e797ee
Jaikumar Lohana <81681673+JaiLohana16@users.noreply.github.com>
2024-11-02 14:06:07 +0500

    Update userController.js

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -55,7 +55,7 @@ export const registerUser = async (req, res) => {
 export const adminLogin = async (req, res) => {
     try {
         const { email, password } = req.body;
-        if (email === "lohana@evergreen.com" && password === "Lohana@123") {
+        if (email ===  process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
             const token = jwt.sign({ email }, process.env.JWT_SECRET);
             res.json({ success: true, token });
         } else {
