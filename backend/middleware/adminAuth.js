746b992fcf4fdac3ad00477357f0380535d707a5
Jaikumar Lohana <81681673+JaiLohana16@users.noreply.github.com>
2024-11-02 16:15:50 +0500

    Update adminAuth.js

--- backend/middleware/adminAuth.js
+++ backend/middleware/adminAuth.js
@@ -1,23 +1,20 @@
-import jwt from "jsonwebtoken"
+const adminAuth = async (req, res, next) => {
+  try {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Extract token after "Bearer"
 
-const adminAuth =async (req,res,next) => {
-    try {
-        const {token} =req.headers
-
-        if (!token) {
-            return res.json({success:false,message:"Not Authorized Admin"})
-        }
-        else{
-            const token_decode=jwt.verify(token,process.env.JWT_SECRET)
-            if (token_decode !==process.env.ADMIN_EMAIL+process.env.ADMIN_PASSWORD) {
-                return res.json({success:false,message:"Not Authorized"})  
-            }
-           next()
-        }
-
-    } catch (error) {
-        res.json({success:false,message:error.message})
+    if (!token) {
+      return res.json({ success: false, message: "Not Authorized Admin" });
+    } else {
+      const token_decode = jwt.verify(token, process.env.JWT_SECRET);
+      if (token_decode !== process.env.ADMIN_EMAIL + process.env.ADMIN_PASSWORD) {
+        return res.json({ success: false, message: "Not Authorized" });
+      }
+      next();
     }
-}
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
 
-export default adminAuth
+export default adminAuth;
