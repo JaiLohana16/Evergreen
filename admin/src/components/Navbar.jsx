6a4fd4d7fb8559c76e65ed267d13fbfc0f1425ea
Jaikumar Lohana <81681673+JaiLohana16@users.noreply.github.com>
2024-11-02 17:08:22 +0500

    Update Navbar.jsx

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -8,7 +8,8 @@ const Navbar = ({setToken}) => {
   return (
     
     <div className='flex items-center py-2 px-[4%] justify-between bg-[#ffebf5]'>
-      <NavLink to="/"><img className='w-[max(20%,150px)]' src={assets.logo} alt="logo" /></NavLink>
+      <NavLink to="/"><p className="text-3xl text-green-500">EVERGREEN</p></NavLink>
+      <p className="text-xl text-green-200">Admin Panel</p>
       <button onClick={handleLogout} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
   )
