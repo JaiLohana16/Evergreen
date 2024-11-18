f95105c188770fd0fcee76505d5c82ddc3cc58b9
Jaikumar Lohana <81681673+JaiLohana16@users.noreply.github.com>
2024-11-18 17:44:40 +0500

    Update SearchBar.jsx

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -9,7 +9,7 @@ const SearchBar = () => {
     
   return (
     <div>
-      {showSearch &&location.pathname.includes("collection")  ?<div className='border-t border-b bg-gray-50 text-center'>
+      {showSearch &&location.pathname.includes("collection" || "mobilecollection")  ?<div className='border-t border-b bg-gray-50 text-center'>
         <div className='inline-flex items-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 '>
         <input value={search} onChange={(e)=>setSearch(e.target.value)} type="text" placeholder='Search' className='flex-1 outline-none bg-inherit text-sm' />
         <img src={assets.search_icon} alt="searchicon" className='w-4' />
